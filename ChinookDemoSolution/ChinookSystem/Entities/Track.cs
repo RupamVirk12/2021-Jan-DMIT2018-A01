c93406adf342c2bc929b4b5393560563fd779ae9
RupamVirk12 <71781100+RupamVirk12@users.noreply.github.com>
2021-01-15 16:50:08 -0700

    Updated all Nuget packages, Add EntityFramework to library, check all references, replaces menu and added class MediaType, Genre and Track to entities

---
+++ ChinookDemoSolution/ChinookSystem/Entities/Track.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Tracks")]
+    internal class Track
+    {
+
+        private string _Composer;
+        [Key]
+        public int TrackId { get; set; }
+
+        [StringLength(200, ErrorMessage = "Track name is limited to 200 characters.")]
+        [Required(ErrorMessage = "Track Name is required.")]
+        public string Name { get; set; }
+
+        public int? AlbumId { get; set; }
+
+        [Required(ErrorMessage = "MediaTypeId is required.")]
+        public int MediaTypeId { get; set; }
+
+        public int? GenreId{ get; set; }
+
+        [StringLength(220, ErrorMessage = "Composer is limited to 220 characters.")]
+        public string Composer
+        {
+            get { return _Composer; }
+            set { _Composer = string.IsNullOrEmpty(value)?null:value; }
+
+        }
+
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+
+    }
+}
